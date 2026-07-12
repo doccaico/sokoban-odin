7bfcce59f70bd953c4936cbf84ff3d3d639a1c76
doccaico <hash_ext_luna@yahoo.co.jp>
2026-07-12 13:46:51 +0900

    Up

--- web/index.js
+++ web/index.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\doccaico\AppData\Local\Temp\tmpnglyvyrw.js
+// include: C:\Users\doccaico\AppData\Local\Temp\tmpbpmwtr69.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -205,21 +205,21 @@ Module['FS_createPath']("/assets", "button", true, true);
 
   })();
 
-// end include: C:\Users\doccaico\AppData\Local\Temp\tmpnglyvyrw.js
-// include: C:\Users\doccaico\AppData\Local\Temp\tmpz46dgpwd.js
+// end include: C:\Users\doccaico\AppData\Local\Temp\tmpbpmwtr69.js
+// include: C:\Users\doccaico\AppData\Local\Temp\tmprshbzmy2.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\doccaico\AppData\Local\Temp\tmpz46dgpwd.js
-// include: C:\Users\doccaico\AppData\Local\Temp\tmp80btga99.js
+  // end include: C:\Users\doccaico\AppData\Local\Temp\tmprshbzmy2.js
+// include: C:\Users\doccaico\AppData\Local\Temp\tmpfw3b30f4.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\doccaico\AppData\Local\Temp\tmp80btga99.js
+  // end include: C:\Users\doccaico\AppData\Local\Temp\tmpfw3b30f4.js
 
 
 var arguments_ = [];
